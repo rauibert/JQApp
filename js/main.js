7922462df7fc0ecdd0e13600a91a4c673a5317cd
Rauibert <rauibertnf@gmail.com>
2020-05-05 18:58:40 +0200

    Read JSON with $.GetJSON

--- js/main.js
+++ js/main.js
@@ -1,4 +1,28 @@
 
 $(document).ready(function(){
-    console.log("Jquery cargado");
+    
+    //Variables
+    var search_results = $("#search_results");
+
+
+    //Load JSON
+    var getJSON = $.getJSON("db/films.json", function(data){ 
+        
+        console.log(data);
+
+        var films = data.film;
+
+        for(var film in films){
+            search_results.append(
+                "<p class= 'film_box'> Name: "+ films[film].name + "</p>" +
+                "<p class= 'film_box'> Year: "+ films[film].year + "</p>" +
+                "<p class= 'film_box'> Director: "+ films[film].director + "</p>" +
+                "<p class= 'film_box'> Screenwriter: "+ films[film].screenwriter + "</p>" +
+                "<p class= 'film_box'> Story by: "+ films[film].stroyby + "</p>" 
+                );
+        }
+
+    });
+
+
 });
