9604f8837bacf64c52f483400fd2d0888bad0b34
Rauibert <rauibertnf@gmail.com>
2020-05-06 00:58:42 +0200

    Create a branch with only pure javascript

--- js/main.js
+++ js/main.js
@@ -1,38 +1,42 @@
+'use strict';
 
-$(document).ready(function(){
-    
+window.addEventListener('load', function(){
+
     //Variables
-    var search_results = $("#search_results");
-
+    var search_results = document.querySelector('#search_results');
 
     //Load JSON
-    var getJSON = $.getJSON("db/films.json", function(data){ 
+    this.fetch('db/films.json')
+        .then(data => data.json())
+        .then(films =>{
+            console.log(films);
+            var filmsData = films.film;
+            
+
+            for(var film in filmsData){
+                
+                search_results.innerHTML +='<p class= "film_box"> Name: '+ filmsData[film].name + '<br>' +
+                    'Year: '+ filmsData[film].year + '<br>' +
+                    'Director: '+ filmsData[film].director + '<br>' +
+                    'Screenwriter: '+ filmsData[film].screenwriter + '<br>' +
+                    'Story by: '+ filmsData[film].stroyby + '</p>';
+                    
+            }
+
+            var film_box = document.querySelectorAll(".film_box");
+
+            for(var i = 0; i<film_box.length; i++){
+            
+                film_box[i].addEventListener("mouseenter", function(){
+                    this.style.background = "black";
+                    this.style.color = "white";
+                });
         
-        var films = data.film;
-
-        for(var film in films){
-            search_results.append(
-                "<p class= 'film_box'> Name: "+ films[film].name + "<br>" +
-                "Year: "+ films[film].year + "<br>" +
-                "Director: "+ films[film].director + "<br>" +
-                "Screenwriter: "+ films[film].screenwriter + "<br>" +
-                "Story by: "+ films[film].stroyby + "</p>" 
-                );
-        }
-
-        $(".film_box").on("mouseenter", function(){
-            $(this).css("background","black")
-                .css("color", "white");
+                film_box[i].addEventListener("mouseleave", function(){
+                    this.style.background = "rgb(152, 188, 243)";
+                    this.style.color = "black";
+                }); 
+            };   
         });
-
-        $(".film_box").on("mouseleave", function(){
-            $(this).css("background","white")
-                .css("color", "black");
-        });
-    
-
-    });
-
-    
-
 });
+
